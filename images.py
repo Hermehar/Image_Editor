da5a5ea512995744209b08ef16d775be98e01b50
Hermehar <63737394+Hermehar@users.noreply.github.com>
2021-05-21 13:40:01 +0530

    Update images.py

--- images.py
+++ images.py
@@ -13,7 +13,7 @@ def redscreen(main_filename, back_filename):                                    
     back = SimpleImage(back_filename)
     for pixel in image:
         average = (pixel.red + pixel.green + pixel.blue) // 3
-        if pixel.red >= average * INTENSITY_THRESHOLD:                                    """it helps to merge two different images"""
+        if pixel.red >= average * INTENSITY_THRESHOLD:                                   
             x = pixel.x
             y = pixel.y
             image.set_pixel(x, y, back.get_pixel(x, y))
@@ -22,13 +22,13 @@ def redscreen(main_filename, back_filename):                                    
 def darker(image):                                                                      """gets you the darker version of the image"""
     for pixel in image:
         pixel.red = pixel.red // 2
-        pixel.green = pixel.green // 2                                                """RGB pixel values are to be decreased"""
+        pixel.green = pixel.green // 2                                                
         pixel.blue = pixel.blue // 2
 
 def red_channel(filename):                                                         """converts the image into red color"""
     for pixel in filename:
         pixel.green = 0
-        pixel.blue = 0                                                             """put the values of green and blue pixels to 0"""
+        pixel.blue = 0                                                             
     return filename
 
 def compute_luminosity(red, green, blue):
@@ -49,17 +49,18 @@ def mirror_hor(filename):                                                       
     for x in range (width):
         for y in range (height):
             pixel = filename.get_pixel(x, y)
-            mirror.set_pixel(x, y, pixel)
-            mirror.set_pixel((width *2) - (x+1) ,y ,pixel)
-    return filename
+            mirror_hor.set_pixel(x, y, pixel)
+            mirror_hor.set_pixel((width *2) - (x+1) ,y ,pixel)
+    return mirror_hor
 
-def mirror_ver(filename):                                                     """gets you the vertical mirror image of the image"""
+def mirror_ver(filename):                                                         """gets you the vertical mirror image of the image"""
+    mirror_ver = SimpleImage.blank(width, height*2)
     for x in range (width):
           for y in range (height):
             pixel = filename.get_pixel(x, y)
-            mirror.set_pixel(x, y, pixel)
-            mirror.set_pixel(x,(height*2)-(y+1),pixel)
-    return filename
+            mirror_ver.set_pixel(x, y, pixel)
+            mirror_ver.set_pixel(x,(height*2)-(y+1),pixel)
+    return mirror_ver
 
 def main():
     filename = get_file()
